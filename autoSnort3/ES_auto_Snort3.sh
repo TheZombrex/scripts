b19c22f12a3c96acae675882b82ef793194bb048
TheZombrex <davidfernandezperez.thezombrex@gmail.com>
2022-07-28 01:52:46 +0200

    Minor changes

--- autoSnort3/ES_auto_Snort3.sh
+++ autoSnort3/ES_auto_Snort3.sh
@@ -21,7 +21,7 @@ echo -e "$indicador Se van a instalar las dependencias"
 echo -e "		Las dependencias son: "
 echo -e "			build-essential libpcap-dev libpcre3-dev libnet1-dev zlib1g-dev luajit hwloc libdnet-dev libdumbnet-dev bison flex liblzma-dev openssl libssl-dev pkg-config libhwloc-dev cmake cpputest libsqlite3-dev uuid-dev libcmocka-dev libnetfilter-queue-dev libmnl-dev autotools-dev libluajit-5.1-dev libunwind-dev"
 
-sudo apt install build-essential libpcap-dev libpcre3-dev libnet1-dev zlib1g-dev luajit hwloc libdnet-dev libdumbnet-dev bison flex liblzma-dev openssl libssl-dev pkg-config libhwloc-dev cmake cpputest libsqlite3-dev uuid-dev libcmocka-dev libnetfilter-queue-dev libmnl-dev autotools-dev libluajit-5.1-dev libunwind-dev
+sudo apt install build-essential libpcap-dev libpcre3-dev libnet1-dev zlib1g-dev luajit hwloc libdnet-dev libdumbnet-dev bison flex liblzma-dev openssl libssl-dev pkg-config libhwloc-dev cmake cpputest libsqlite3-dev uuid-dev libcmocka-dev libnetfilter-queue-dev libmnl-dev autotools-dev libluajit-5.1-dev libunwind-dev -y
 
 echo
 echo -e "$indicador Instalando libdaq"
